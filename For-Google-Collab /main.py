cc65487b959d7bf34380c36c11f1cc623a027004
Kunj Chandak <147203563+Kunj05@users.noreply.github.com>
2025-03-17 01:35:08 +0530

    Create main.py

---
+++ For-Google-Collab /main.py
@@ -0,0 +1,7 @@
+import os
+
+# Set your LinkedIn token (LI_AT cookie) as an environment variable
+os.environ['LI_AT_COOKIE'] = '-8NS56RP2iJM'  # Replace this with your actual LI_AT cookie value
+
+# Run the script to start scraping job listings
+!python python.py
